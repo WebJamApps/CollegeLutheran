e103f1d7c274811ee05afd8a82055c7fab6c9247
Rhayven <codingforllamas@gmail.com>
2019-12-19 12:20:42 -0500

    Makes music page
    Makes the music page look like collegelutheran.org

--- src/containers/Music/index.js
+++ src/containers/Music/index.js
@@ -1,15 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import PicSlider from '../../components/pic-slider';
-import DefaultTable from '../../components/tour-table';
-import JoshBio from './joshBio';
-import MariaBio from './mariaBio';
-import EmersonBio from './emersonBio';
-import BrianBio from './brianBio';
-import Wjband from './wjBand';
-import Intro from './intro';
-import mapStoreToProps from '../../redux/mapStoreToProps';
 
 export class Music extends Component {
   constructor(props) {
@@ -19,59 +8,47 @@ export class Music extends Component {
 
   componentDidMount() { document.title = 'Music | Web Jam LLC'; }
 
-  musicians() { // eslint-disable-line class-methods-use-this
-    return (
-      <div className="elevation3" style={{ maxWidth: '1000px', margin: 'auto' }}>
-        <section>
-          <JoshBio />
-          <hr />
-          <MariaBio />
-          <hr />
-          <Wjband />
-          <hr />
-          <EmersonBio />
-          <hr />
-          <BrianBio />
-        </section>
-        <p>{' '}</p>
-        <p>{' '}</p>
-      </div>
-    );
-  }
-
   render() {
-    const { images } = this.props;
     return (
       <div className="page-content">
-        <div style={{ paddingTop: '1px', paddingBottom: 0, marginBottom: 0 }}>
-          <div id="musicSlide1">
-            {images.length > 0 ? (<PicSlider data={images} />) : null}
+        <div className="container-fluid">
+          <div>
+            <div id="musicSlide1">
+              <img alt="churchBalcony" src="https://dl.dropboxusercontent.com/s/g8lhfjk64uixl9r/choir.png?dl=0" />
+            </div>
           </div>
-          <Intro />
+          <div className="material-content elevation2" style={{ maxWidth: '998px', margin: 'auto' }}>
+            <h3 style={{ paddingBottom: '15px', fontWeight: '100' }}>Music at College Lutheran</h3>
+            <p>
+          Music Ministry at College Lutheran involves all members of the congregation as they participate in the hymns, songs, psalms, acclamations,
+          and musical responses of the liturgy. There are also members who sing in the choir, play an
+          instrument, provide special music at times, or supply names of others within the community to contact as guest musicians.
+          We sing and play to the glory of God.
+            </p>
+            <p>
+          Currently, the Adult Choir sings from September through Pentecost Sunday. We warm up and prepare on Sunday mornings before worship and
+          practice for about an hour after worship for the upcoming Sundays and festivals. The choir generally has
+          one Sunday off a month, which is when guest musicians are invited to join us. Instrumentalists such as guitar, winds, brass, strings,
+          often accompany the choir anthem as well. And we especially try to highlight and include our youth
+          musicians from CLC and from the community. We are always looking to include new instrumentalists,
+          and we also invite any vocalists to join us in the choir.
+            </p>
+            <p>
+          College Lutheran Church is home to 5 octaves of handbells. But we need many hands to make a bell choir possible again so please consider
+          this music ministry option. It is helpful if you read music but not necessary; if you enjoy music and
+          can count, this could be a wonderful new experience and would bring the bells back to re-sounding joy in
+          our church, to the delight of everyone here.
+            </p>
+            <p>
+          The Sunday School children provide a song for us during worship periodically and present the annual Advent/Christmas program in December.
+          Music ministry participates in other congregational programs such as Trunk or Treat, carol and hymn
+          sings, a sing-along at the parish picnic, etc.
+            </p>
+          </div>
         </div>
-        <div
-          className="search-table-outer"
-          style={{
-            position: 'relative', overflowX: 'auto', maxWidth: '96%', margin: 'auto',
-          }}
-        >
-          <DefaultTable />
-        </div>
-        <div style={{ height: '10px' }}>
-          <p>{' '}</p>
-        </div>
-        {this.musicians()}
       </div>
     );
   }
 }
 
-Music.defaultProps = { images: [] };
-Music.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.shape({
-    url: PropTypes.string,
-    title: PropTypes.string,
-  })),
-};
-
-export default connect(mapStoreToProps)(Music);
+export default Music;
--- test/containers/music.spec.js
+++ test/containers/music.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Music } from '../../src/containers/Music';
-import PicSlider from '../../src/components/pic-slider';
 
 function setup(data) {
   let wrapper;
@@ -15,20 +14,5 @@ describe('/music', () => {
   it('renders the component', () => {
     const { wrapper } = setup();
     expect(wrapper.find('div.page-content').exists()).toBe(true);
-    expect(wrapper.find('Intro').dive().find('.intro').exists()).toBe(true);
-    expect(wrapper.find('JoshBio').dive().find('.joshBio').exists()).toBe(true);
-    expect(wrapper.find('JoshBio').dive().find('Instruments').dive()
-      .find('.instruments')
-      .exists()).toBe(true);
-    expect(wrapper.find('MariaBio').dive().find('.mariaBio').exists()).toBe(true);
-    expect(wrapper.find('Wjband').dive().find('.wjBand').exists()).toBe(true);
-    expect(wrapper.find('EmersonBio').dive().find('.emersonBio').exists()).toBe(true);
-    expect(wrapper.find('BrianBio').dive().find('.brianBio').exists()).toBe(true);
   });
-  it('renders with images', () => new Promise((done) => {
-    const data = [{}];
-    const wrapper2 = shallow(<Music images={data} />);
-    expect(wrapper2.find(PicSlider).exists()).toBe(true);
-    done();
-  }));
 });
