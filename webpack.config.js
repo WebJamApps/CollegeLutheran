a03d11138bb4a99b596905fb104572fafdea172d
Joshua Sherman <jsherman@valcom.com>
2020-01-06 07:52:44 -0500

    adds prodfile and rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const ensureArray = (config) => config && (Array.isArray(config) ? config : [con
 const when = (condition, config, negativeConfig) => (condition ? ensureArray(config) : ensureArray(negativeConfig));
 
 // primary config:
-const title = 'Web Jam LLC';
+const title = 'College Lutheran Church';
 const outDir = path.resolve(__dirname, 'dist');
 const srcDir = path.resolve(__dirname, 'src');
 const nodeModulesDir = path.resolve(__dirname, 'node_modules');
