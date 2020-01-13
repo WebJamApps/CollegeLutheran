11dd631eb1ed925ab94a7cc8efb730fe701a29b5
Joshua Sherman <jsherman@valcom.com>
2020-01-13 08:58:48 -0500

    more refactoring from html to jsx

--- src/containers/StaffDashboard/index.js
+++ src/containers/StaffDashboard/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import moment from 'moment';
-import { withRouter, Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+// import { withRouter, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
@@ -10,7 +11,7 @@ export class StaffDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirect: false,
+      // redirect: false,
       // date: '',
       // time: '',
       // tickets: '',
@@ -81,155 +82,173 @@ export class StaffDashboard extends Component {
   // }
 
   render() {
-    const { redirect } = this.state;
+    // const { redirect } = this.state;
     // const {
     //   date, time, buttonStyle, redirect,
     // } = this.state;
     return (
       <div className="page-content">
         <h3>CLC Admin Dashboard</h3>
-          <div className="material-content elevation3">
-            <h4 class="material-header-h4">Add Monthly Forum</h4>
-            <form>
-              <label>Type
-              <select id="mediaType" className="form-control" value="">
-                <option></option>
-              </select>
-              </label>
-              <label>Dropbox URL to PDF
-              <input value=""/></label>
-              <div className="form-group">
-                <ul>
-                <li style={{color:red}}>
-                  </li>
-                </ul>
-              </div>
-              <button id="createMediaButton" class="button-lib">
+        <div className="material-content elevation3">
+          <h4 className="material-header-h4">Add Monthly Forum</h4>
+          <form>
+            <label htmlFor="dropbox-url">
+Dropbox URL to PDF
+              <input id="dropbox-url" value="" />
+            </label>
+            <div className="form-group">
+              <ul>
+                <li style={{ color: 'red' }} />
+              </ul>
+            </div>
+            <button type="button" id="createMediaButton" className="button-lib">
                 Submit
-              </button>
-            </form>
-          </div>
-              <hr>
-          <div class="material-content elevation3" style="max-width:5in; margin:auto">
-            <h4 class="material-header-h4">Delete Monthly Forum</h4>
-            <form>
-              <label style="padding-top:0">Select</label>
-              <select id="selectBookTitle" className="form-control" value="">
-                <option value=""></option>
-                <option value="book._id"></option>
-              </select>
-              <button className="button-lib">
-                Delete
-              </button>
-            </form>
-          </div>
-                    <hr>
-          <div className="material-content elevation3">
-            <h4 class="material-header-h4">Change Homepage Section</h4>
-            <form>
-              <label>Title
-              <input value="homePageContent.title"/>
-              </label>
-              <label>Content
-              <textarea rows="15" cols="32" value="homePageContent.comments"/></label>
-              <button id="changeStuff" className="button-lib">
-                Submit
-              </button>
-            </form>
-          </div>
-          <hr>
-          <div class="material-content elevation3" style="max-width:5in; margin:auto">
-            <h4 class="material-header-h4">Change Youthpage Section</h4>
-            <form>
-              <label>Content
-              <textarea rows="15" cols="32" value="youthPageContent.comments"/></label>
-              <button id="changeYouth" class="button-lib">
-                Submit
-              </button>
-            </form>
-            <hr>
-            <h4 class="material-header-h4">Add Youthpage Picture from Dropbox</h4>
-            <form>
-              <label>Dropbox URL to Picture
-              <input value="newBook.url"/></label>
-              <button id="createYouthPic" className="button-lib">
-                Add Pic
-              </button>
-            </form>
-            <hr>
-            <h4 class="material-header-h4">Add Youthpage Pic from Image Address</h4>
-            <form>
-              <label>Title
-              <input value="newYouthPic.title"/></label>
-              <label>Image Address
-              <input value="newYouthPic.url"/></label>
-              <button id="addYouthPic" className="button-lib">
-                Add Pic
-              </button>
-            </form>
-            <p style={{color:'red'}}><strong>errorMessage</strong></p>
-            <hr>
-            <h4 className="material-header-h4">Delete Youthpage Picture</h4>
-            <form>
-              <label>Select
-              <select id="selectYouthPic" className="form-control" value="titleSelected">
-                <option value=""></option>
-                <option value="pic._id"></option>
-              </select>
-              </label>
-              <button id="deleteYouth" className="button-lib">
-                Delete
-              </button>
-            </form>
-          </div>
-          <p style="font-size:2pt">&nbsp;</p>
-          <div class="material-content elevation3" style="max-width:5in; margin:auto">
-            <h4 class="material-header-h4">Change Familypage Section</h4>
-            <form>
-              <label>Content</label>
-              <textarea rows="15" cols="32" value.two-way="familyPageContent.comments"></textarea>
-              <button id="changeFamily" class="button-lib" raised click.trigger="changeFamilyPage()">
-                Submit
-              </button>
-            </form>
-            <p>&nbsp;</p>
-            <hr>
-            <h4 class="material-header-h4">Add Familypage Pic from Dropbox</h4>
-            <form>
-              <label>Dropbox URL to Picture</label>
-              <input value.two-way="newBook.url & validate">
-              <button id="createFamilyPic" class="button-lib" style="width:70px" raised click.trigger="createFamilyPic()">
-                Add Pic
-              </button>
-            </form>
-            <p>&nbsp;</p>
-            <hr>
-            <h4 class="material-header-h4">Add Familypage Pic from Image Address</h4>
-            <form>
-              <label>Title</label>
-              <input value.two-way="newFamilyPic.title" style="min-width:0" required>
-              <label>Image Address</label>
-              <input value.two-way="newFamilyPic.url">
-              <button id="addFamilyPic" class="button-lib" style="width:70px" raised click.trigger="addFamilyPic()">
-                Add Pic
-              </button>
-            </form>
-            <p style="color:red;"><strong>${familyPicError}</strong></p>
-            <p>&nbsp;</p>
-            <hr>
-            <h4 class="material-header-h4">Delete Familypage Picture</h4>
-            <form>
-              <label>Select</label>
-              <select id="selectFamilyPic" class="form-control" change.delegate="showDelete()" value.bind="titleSelected" style="margin-top:0;max-width: 2.5in;overflow-wrap:break-word;word-break:break-all;">
-                <option value=""></option>
-                <option repeat.for="pic of familyPicsArr" value="${pic._id}" model.bind="familyPicsArr">${pic.title}</option>
-              </select>
-              <button if.bind="showDeleteButton" id="deleteFamily" class="button-lib" raised click.trigger="deleteFamilyPic()">
-                Delete
-              </button>
-            </form>
-          </div>
+            </button>
+          </form>
         </div>
+        <hr />
+        <div className="material-content elevation3">
+          <h4 className="material-header-h4">Delete Monthly Forum</h4>
+          <form>
+            <label htmlFor="selectBookTitle">
+Select
+              {/* <select id="selectBookTitle" className="form-control" value="" /> */}
+            </label>
+            <button type="button" className="button-lib">
+                Delete
+            </button>
+          </form>
+        </div>
+        <hr />
+        <div className="material-content elevation3">
+          <h4 className="material-header-h4">Change Homepage Section</h4>
+          <form>
+            <label htmlFor="title">
+Title
+              <input id="title" value="homePageContent.title" />
+            </label>
+            <label htmlFor="content">
+Content
+              <textarea id="content" rows="15" cols="32" value="homePageContent.comments" />
+            </label>
+            <button type="button" id="changeStuff" className="button-lib">
+                Submit
+            </button>
+          </form>
+        </div>
+        <hr />
+        <div className="material-content elevation3">
+          <h4 className="material-header-h4">Change Youthpage Section</h4>
+          <form>
+            <label htmlFor="youth-content">
+Content
+              <textarea if="youth-content" rows="15" cols="32" value="youthPageContent.comments" />
+            </label>
+            <button type="button" id="changeYouth" className="button-lib">
+                Submit
+            </button>
+          </form>
+          <hr />
+          {/* <h4 className="material-header-h4">Add Youthpage Picture from Dropbox</h4>
+          <form>
+            <label htmlFor="youth-pic">
+Dropbox URL to Picture
+              <input id="youth-pic" value="newBook.url" />
+            </label>
+            <button type="button" id="createYouthPic" className="button-lib">
+                Add Pic
+            </button>
+          </form>
+          <hr /> */}
+          <h4 className="material-header-h4">Add Youthpage Pic from Image Address</h4>
+          <form>
+            <label htmlFor="youth-pic-title">
+Title
+              <input id="youth-pic-title" value="newYouthPic.title" />
+            </label>
+            <label htmlFor="youth-pic-url">
+Image Address
+              <input id="youth-pic-url" value="newYouthPic.url" />
+            </label>
+            <button type="button" id="addYouthPic" className="button-lib">
+                Add Pic
+            </button>
+          </form>
+          <p style={{ color: 'red' }}><strong>errorMessage</strong></p>
+          <hr />
+          <h4 className="material-header-h4">Delete Youthpage Picture</h4>
+          <form>
+            <label htmlFor="delete-youth-pic">
+Select
+              <select id="delete-youth-pic" className="form-control" value="titleSelected">
+                {/* <option value="" />
+                <option value="pic._id" /> */}
+              </select>
+            </label>
+            <button type="button" id="deleteYouth" className="button-lib">
+                Delete
+            </button>
+          </form>
+        </div>
+        <div className="material-content elevation3">
+          <h4 className="material-header-h4">Change Familypage Section</h4>
+          <form>
+            <label htmlFor="family-content">
+Content
+              <textarea id="family-content" rows="15" cols="32" value="familyPageContent.comments" />
+            </label>
+            <button type="button" id="changeFamily" className="button-lib">
+                Submit
+            </button>
+          </form>
+          <hr />
+          {/* <h4 className="material-header-h4">Add Familypage Pic from Dropbox</h4>
+          <form>
+            <label>
+Dropbox URL to Picture
+              <input value="newBook.url" />
+            </label>
+            <button id="createFamilyPic" className="button-lib" style="width:70px">
+                Add Pic
+            </button>
+          </form>
+          <hr /> */}
+          <h4 className="material-header-h4">Add Familypage Pic from Image Address</h4>
+          <form>
+            <label>
+Title
+              <input value="newFamilyPic.title" style="min-width:0" />
+            </label>
+            <label>
+Image Address
+              <input value="newFamilyPic.url" />
+            </label>
+            <button id="addFamilyPic" className="button-lib" style="width:70px">
+                Add Pic
+            </button>
+          </form>
+          <p style="color:red;">
+            <strong>
+$
+              {familyPicError}
+            </strong>
+          </p>
+          <hr />
+          <h4 className="material-header-h4">Delete Familypage Picture</h4>
+          <form>
+            <label>
+Select
+              <select id="selectFamilyPic" className="form-control">
+                <option value="" />
+                <option value="pic._id">pic.title</option>
+              </select>
+            </label>
+            <button id="deleteFamily" className="button-lib">
+                Delete
+            </button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
