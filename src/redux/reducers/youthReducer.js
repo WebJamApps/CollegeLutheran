3c16a5ba9ad658c1e667a8ace6a2678171b9b430
Rhayven <codingforllamas@gmail.com>
2020-01-14 01:43:35 -0500

    Fixes youth reducer

--- src/redux/reducers/youthReducer.js
+++ src/redux/reducers/youthReducer.js
@@ -2,24 +2,10 @@ const initialState = {
   youthPics: [],
 };
 
-const fixUrl = (state, action) => {
-  let picArr = [];
-  if (Array.isArray(action.data)) picArr = action.data;
-  const youthPics = picArr.map((i) => {
-    const newPic = i;
-    newPic.url = i.comments;
-    return newPic;
-  });
-  return {
-    ...state,
-    youthPics,
-  };
-};
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GOT_YOUTHPICS':
-      return fixUrl(state, action);
+      return { ...state, youthPics: action.data };
     default:
       return state;
   }
