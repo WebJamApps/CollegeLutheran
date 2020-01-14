efbacbd130104d19af44645712f2718d0715d2ab
Joshua Sherman <jsherman@valcom.com>
2020-01-14 09:25:58 -0500

    wip admin form

--- src/containers/StaffDashboard/index.js
+++ src/containers/StaffDashboard/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import moment from 'moment';
 import { withRouter } from 'react-router-dom';
 // import { withRouter, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -11,18 +10,10 @@ export class StaffDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // redirect: false,
-      // date: '',
-      // time: '',
-      // tickets: '',
-      // more: '',
-      // venue: '',
+      // redirect: '',
     };
     this.forms = forms;
     this.onChange = this.onChange.bind(this);
-    // this.createTour = this.createTour.bind(this);
-    // this.validateForm = this.validateForm.bind(this);
-    // this.createTourApi = this.createTourApi.bind(this);
   }
 
   componentDidMount() { document.title = 'Staff Dashboard | College Lutheran Church'; }
@@ -40,78 +31,33 @@ export class StaffDashboard extends Component {
   //   return true;
   // }
 
-  // createTourApi(tour1) { // eslint-disable-line class-methods-use-this
-  //   const { scc, auth } = this.props;
-  //   const tour = tour1;
-  //   tour.datetime = new Date(tour.date);
-  //   const m = moment(tour.date, 'YYYY-MM-DD');
-  //   tour.date = m.format('ll');
-  //   scc.emit('newTour', { tour, token: auth.token });
-  //   this.setState({ redirect: true });
-  //   return true;
-  // }
-
-  // createTour() {
-  //   const {
-  //     date, time, location, venue, tickets, more,
-  //   } = this.state;
-  //   const tour = {
-  //     date, time, location, venue, tickets, more,
-  //   };
-  //   // return this.createTourApi(tour);
-  // }
-
-  // newTourForm(date, time, buttonStyle) {
-  //   const {
-  //     venue, location, tickets, more,
-  //   } = this.state;
-  //   return (
-  //     <form id="new-tour" style={{ marginTop: '4px', paddingLeft: '10px' }}>
-  //       {this.forms.makeInput('date', 'Date', true, this.onChange, date)}
-  //       {this.forms.makeInput('text', 'Time', true, this.onChange, time)}
-  //       {this.forms.makeInput('text', 'Venue', true, this.onChange, venue)}
-  //       {this.forms.makeInput('text', 'Location', true, this.onChange, location)}
-  //       {this.forms.makeInput('text', 'Tickets', false, this.onChange, tickets)}
-  //       {this.forms.makeInput('text', 'More', false, this.onChange, more)}
-  //       <div style={{ textAlign: 'right', marginTop: '10px', maxWidth: '85%' }}>
-  //         <span style={{ fontSize: '16px', marginRight: '38%', fontFamily: 'Habibi' }}><i>* Required</i></span>
-  //         <button style={buttonStyle} disabled={this.validateForm()} type="button" onClick={this.createTour}>Create Tour</button>
-  //       </div>
-  //     </form>
-  //   );
-  // }
-
   render() {
     // const { redirect } = this.state;
-    // const {
-    //   date, time, buttonStyle, redirect,
-    // } = this.state;
     return (
       <div className="page-content">
-        <h3>CLC Admin Dashboard</h3>
-        <div className="material-content elevation3">
-          <h4 className="material-header-h4">Add Monthly Forum</h4>
+        <h4 style={{ textAlign: 'center', marginTop: '10px' }}>CLC Admin Dashboard</h4>
+        <div className="material-content elevation3" style={{ maxWidth: '320px', margin: 'auto' }}>
+          <h5>Add Monthly Forum</h5>
           <form>
             <label htmlFor="dropbox-url">
 Dropbox URL to PDF
               <input id="dropbox-url" value="" />
             </label>
-            <div className="form-group">
+            {/* <div className="form-group">
               <ul>
                 <li style={{ color: 'red' }} />
               </ul>
-            </div>
+            </div> */}
             <button type="button" id="createMediaButton" className="button-lib">
                 Submit
             </button>
           </form>
-        </div>
-        <hr />
-        <div className="material-content elevation3">
-          <h4 className="material-header-h4">Delete Monthly Forum</h4>
+          <hr />
+          <h5>Delete Monthly Forum</h5>
           <form>
             <label htmlFor="selectBookTitle">
 Select
+              <br />
               {/* <select id="selectBookTitle" className="form-control" value="" /> */}
             </label>
             <button type="button" className="button-lib">
@@ -119,24 +65,31 @@ Select
             </button>
           </form>
         </div>
-        <hr />
-        <div className="material-content elevation3">
-          <h4 className="material-header-h4">Change Homepage Section</h4>
-          <form>
-            <label htmlFor="title">
+        <p>{' '}</p>
+        <div className="material-content elevation3" style={{ maxWidth: '8in', margin: 'auto' }}>
+          <h5>Change Homepage Section</h5>
+          <form style={{
+            textAlign: 'left', marginLeft: '4px', width: '100%', maxWidth: '100%',
+          }}
+          >
+            <label htmlFor="title" style={{ textAlign: 'left' }}>
 Title
-              <input id="title" value="homePageContent.title" />
+              <br />
+              <input id="title" />
             </label>
             <label htmlFor="content">
 Content
-              <textarea id="content" rows="15" cols="32" value="homePageContent.comments" />
+              <br />
+              <textarea id="content" rows="15" value="homePageContent.comments" style={{ width: '90%' }} />
             </label>
-            <button type="button" id="changeStuff" className="button-lib">
+            <div style={{ marginLeft: '60%' }}>
+              <button type="button" id="changeStuff">
                 Submit
-            </button>
+              </button>
+            </div>
           </form>
         </div>
-        <hr />
+
         <div className="material-content elevation3">
           <h4 className="material-header-h4">Change Youthpage Section</h4>
           <form>
@@ -149,17 +102,6 @@ Content
             </button>
           </form>
           <hr />
-          {/* <h4 className="material-header-h4">Add Youthpage Picture from Dropbox</h4>
-          <form>
-            <label htmlFor="youth-pic">
-Dropbox URL to Picture
-              <input id="youth-pic" value="newBook.url" />
-            </label>
-            <button type="button" id="createYouthPic" className="button-lib">
-                Add Pic
-            </button>
-          </form>
-          <hr /> */}
           <h4 className="material-header-h4">Add Youthpage Pic from Image Address</h4>
           <form>
             <label htmlFor="youth-pic-title">
@@ -180,16 +122,15 @@ Image Address
           <form>
             <label htmlFor="delete-youth-pic">
 Select
-              <select id="delete-youth-pic" className="form-control" value="titleSelected">
-                {/* <option value="" />
-                <option value="pic._id" /> */}
-              </select>
+              {/* <select id="delete-youth-pic" className="form-control" value="titleSelected" /> */}
             </label>
             <button type="button" id="deleteYouth" className="button-lib">
                 Delete
             </button>
           </form>
         </div>
+        <p>{' '}
+        {/*
         <div className="material-content elevation3">
           <h4 className="material-header-h4">Change Familypage Section</h4>
           <form>
@@ -202,17 +143,6 @@ Content
             </button>
           </form>
           <hr />
-          {/* <h4 className="material-header-h4">Add Familypage Pic from Dropbox</h4>
-          <form>
-            <label>
-Dropbox URL to Picture
-              <input value="newBook.url" />
-            </label>
-            <button id="createFamilyPic" className="button-lib" style="width:70px">
-                Add Pic
-            </button>
-          </form>
-          <hr /> */}
           <h4 className="material-header-h4">Add Familypage Pic from Image Address</h4>
           <form>
             <label htmlFor="family-pic-title">
@@ -237,16 +167,13 @@ familyPicError
           <form>
             <label htmlFor="delete-family-pic">
 Select
-              <select id="delete-family-pic" className="form-control">
-                {/* <option value="" />
-                <option value="pic._id">pic.title</option> */}
-              </select>
+              <select id="delete-family-pic" className="form-control" />
             </label>
             <button type="button" id="deleteFamily" className="button-lib">
                 Delete
             </button>
           </form>
-        </div>
+        </div> */}
       </div>
     );
   }
