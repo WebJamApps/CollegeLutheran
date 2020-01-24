bf8c0c64d635f8305b13cfba04899980afa418a6
Rhayven <codingforllamas@gmail.com>
2020-01-23 20:00:34 -0500

    refactoring

--- src/redux/reducers/youthReducer.js
+++ src/redux/reducers/youthReducer.js
@@ -2,16 +2,10 @@ const initialState = {
   youthPics: [],
 };
 
-const sortPics = (state, action) => {
-  const youthPics = action.data;
-  youthPics.reverse();
-  return { ...state, youthPics: action.data };
-};
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GOT_YOUTHPICS':
-      return sortPics(state, action);
+      return { ...state, youthPics: action.data.reverse() };
     default:
       return state;
   }
