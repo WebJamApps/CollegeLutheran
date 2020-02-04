4de2f2d8c86c2a53ecc9543c7d160c7f3eb7f0d2
Joshua Sherman <jsherman@valcom.com>
2020-02-04 08:50:48 -0500

    more unit tests

--- test/App/fetch.test.js
+++ test/App/fetch.test.js
@@ -3,7 +3,7 @@ import fetch from '../../src/App/fetch';
 describe('fetch', () => {
   let r;
   it('catches error', async () => {
-    r = await fetch({ props: { dispatch: (fun) => fun }, superagent: { get: () => Promise.reject(new Error('bad')) } }, '/', '');
+    r = await fetch({ props: { dispatch: (fun) => fun }, superagent: { get: () => ({ set: () => Promise.reject(new Error('bad')) }) } }, '/', '');
     expect(r).toBe(false);
   });
 });
--- test/containers/AdminDashboard/index.spec.js
+++ test/containers/AdminDashboard/index.spec.js
@@ -1,57 +1,60 @@
-/* eslint-disable jest/no-commented-out-tests */
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 import { AdminDashboard } from '../../../src/containers/AdminDashboard';
 
 describe('Dashboard Container', () => {
-  // function setup() {
-  //   const auth = { token: '' };
-  //   const props = { auth };
-  //   const wrapper = shallow(<AdminDashboard auth={auth} books={[]} />);
-  //   return { props, wrapper };
-  // }
-  it('is defined', () => {
-    expect(AdminDashboard).toBeDefined();
+  let props, wrapper;
+  beforeEach(() => {
+    props = {
+      auth: { token: 'token' }, books: [{ _id: '123' }], youthPics: [{ _id: '456' }], familyPics: [{ _id: '789' }],
+    };
+    wrapper = shallow(<AdminDashboard auth={props.auth} books={props.books} youthPics={props.youthPics} familyPics={props.familyPics} />);
   });
-//   it('renders correctly', () => {
-//     const { wrapper } = setup();
-//     expect(wrapper).toMatchSnapshot();
-//   });
-//   it('calls on change', () => {
-//     const { wrapper } = setup();
-//     wrapper.instance().setState = jest.fn((boobyJ) => { expect(boobyJ.hi).toBe(11); });
-//     wrapper.instance().onChange({ preventDefault: () => {}, target: { id: 'hi', value: 11 } });
-//   });
-//   // it('catches error when posting home content to backend', async () => {
-//   //   const { wrapper } = setup();
-//   //   wrapper.instance().superagent.post = jest.fn(() => ({ set: () => ({ send: () => Promise.resolve(new Error('bad')) }) }));
-//   //   wrapper.update();
-//   //   wrapper.setState({
-//   //     title: 'New Stuff',
-//   //     homePageContent: 'All this awesome new content!',
-//   //   });
-//   //   await expect(wrapper.instance().createHome()).rejects.toThrow('bad');
-//   // });
-//   // it('creates a new youth pic', async () => {
-//   //  const { wrapper } = setup();
-//   //   wrapper.instance().superagent.post = jest.fn(() => ({ set: () => ({ send: () => Promise.resolve({ status: 201 }) }) }));
-//   //   wrapper.update();
-//   //   wrapper.setState({
-//   //     youthName: 'Some Youngin',
-//   //     youthURL: 'https://www.example.com/totallyapic.png',
-//   //   });
-//   //   const result = await wrapper.instance().createYouthApi();
-//   //   expect(result).toBe(201);
-//   // });
-//   // it('creates a new homepage content', async () => {
-//   //  const { wrapper } = setup();
-//   //   wrapper.instance().superagent.post = jest.fn(() => ({ set: () => ({ send: () => Promise.resolve({ status: 200 }) }) }));
-//   //   wrapper.update();
-//   //   wrapper.setState({
-//   //     title: 'New Stuff',
-//   //     homePageContent: 'All this awesome new content!',
-//   //   });
-//   //   const result = await wrapper.instance().createHome();
-//   //   expect(result).toBe(200);
-//   // });
+  it('renders correctly', () => { expect(wrapper).toMatchSnapshot(); });
+  it('handles click to delete a forum', () => {
+    wrapper.instance().controller.deleteBookApi = jest.fn();
+    wrapper.update();
+    const form = wrapper.instance().deleteForumForm('id', []);
+    const f = shallow(form);
+    f.find('button').simulate('click');
+    expect(wrapper.instance().controller.deleteBookApi).toHaveBeenCalled();
+  });
+  it('handles click to create family pic', () => {
+    wrapper.instance().controller.createPicApi = jest.fn();
+    wrapper.update();
+    const form = wrapper.instance().changeFamilyForm('name', 'url');
+    const f = shallow(form);
+    const createForm = f.find('form').get(0);
+    const button = shallow(createForm);
+    button.find('button#addFamilyPic').simulate('click');
+    expect(wrapper.instance().controller.createPicApi).toHaveBeenCalled();
+  });
+  it('handles click to create youth pic', () => {
+    wrapper.instance().controller.createPicApi = jest.fn();
+    wrapper.update();
+    const createForm = wrapper.find('form').get(3);
+    const button = shallow(createForm);
+    button.find('button#addYouthPic').simulate('click');
+    expect(wrapper.instance().controller.createPicApi).toHaveBeenCalled();
+  });
+  it('handles click to delete family pic', () => {
+    wrapper.instance().controller.deleteBookApi = jest.fn();
+    wrapper.update();
+    const form = wrapper.instance().deleteFamily();
+    const f = shallow(form);
+    f.find('button').simulate('click');
+    expect(wrapper.instance().controller.deleteBookApi).toHaveBeenCalled();
+  });
+  it('sets the stateValue on change', () => {
+    wrapper.instance().setState = jest.fn();
+    wrapper.update();
+    wrapper.instance().onChange({ target: {} }, '');
+    expect(wrapper.instance().setState).toHaveBeenCalled();
+  });
+  it('sets the target value on change', () => {
+    wrapper.instance().setState = jest.fn();
+    wrapper.update();
+    wrapper.instance().onChange({ target: { id: 'youthPicsId', value: '456' } });
+    expect(wrapper.instance().setState).toHaveBeenCalled();
+  });
 });
