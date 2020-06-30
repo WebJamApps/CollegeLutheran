97da6018b4e7ba2d1bf041d31062863c9841a25a
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-06-30 08:20:38 -0400

    finishes removal of ts-ignore

--- test/App/AppTemplate.spec.tsx
+++ test/App/AppTemplate.spec.tsx
@@ -1,15 +1,16 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import React from 'react';
 import { shallow } from 'enzyme';
 import { AppTemplate } from '../../src/App/AppTemplate';
 import authUtils from '../../src/App/authUtils';
 
-const dFunc = () => {};
+const dFunc = () => { };
+const location: any = { pathname: '/' };
+const history: any = {};
+const match: any = {};
 function setup() {
   const props = { children: '<div></div>' };
   document.body.innerHTML = '<div class="page-content"></div>';
-  // @ts-ignore
-  const wrapper = shallow(<AppTemplate dispatch={dFunc} location={{ pathname: '/' }}><div /></AppTemplate>);
+  const wrapper = shallow<AppTemplate>(<AppTemplate match={match} history={history} dispatch={dFunc} location={location}><div /></AppTemplate>);
   return { wrapper, props };
 }
 
@@ -23,107 +24,65 @@ describe('AppTemplate', () => {
     wrapper.setState({ menuOpen: true });
     expect(wrapper.find('div.open').length).toBe(1);
   });
-  it('handles response from google login', () => new Promise((done) => {
-    // @ts-ignore
-    authUtils.responseGoogleLogin = jest.fn(() => true);
-    // @ts-ignore
-    const wrapper2 = shallow(<AppTemplate dispatch={dFunc} location={{ pathname: '/' }}><div /></AppTemplate>);
-    // @ts-ignore
-    const result = wrapper2.instance().responseGoogleLogin({});
+  it('handles response from google login', async () => {
+    authUtils.responseGoogleLogin = jest.fn(() => Promise.resolve(true));
+    const wrapper2 = shallow<AppTemplate>(<AppTemplate dispatch={dFunc} location={location} match={match} history={history}><div /></AppTemplate>);
+    const result = await wrapper2.instance().responseGoogleLogin({});
     expect(result).toBe(true);
-    done();
-  }));
+  });
   it('handles response from google logout', () => {
     authUtils.responseGoogleLogout = jest.fn(() => '');
-    // @ts-ignore
-    const wrapper2 = shallow(<AppTemplate dispatch={dFunc} location={{ pathname: '/' }}><div /></AppTemplate>);
-    // @ts-ignore
-    const result = wrapper2.instance().responseGoogleLogout({});
+    const wrapper2 = shallow<AppTemplate>(<AppTemplate dispatch={dFunc} location={location} match={match} history={history}><div /></AppTemplate>);
+    const result = wrapper2.instance().responseGoogleLogout();
     expect(result).toBe('');
   });
-  it('renders the login button', () => new Promise((done) => {
+  it('renders the login button', () => {
     const { wrapper } = setup();
-    // @ts-ignore
     const loginButton = wrapper.instance().googleButtons('login', 'login');
     const rLogin = shallow(loginButton);
     expect(rLogin.find('div.googleLogin').length).toBe(1);
-    done();
-  }));
-  it('renders the logout button', () => new Promise((done) => {
+  });
+  it('renders the logout button', () => {
     const { wrapper } = setup();
-    // @ts-ignore
     const logoutButton = wrapper.instance().googleButtons('logout', 'logout');
     const rLogout = shallow(logoutButton);
     expect(rLogout.find('div.googleLogout').length).toBe(1);
-    done();
-  }));
-  it('closes the menu without navigating away from the react app', () => new Promise((done) => {
+  });
+  it('closes the menu without navigating away from the react app', () => {
     document.body.innerHTML = '<button class="googleLogin"/><button class="googleLogout"/>';
     const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
-    aT.setState = () => {};
-    // @ts-ignore
-    const result = aT.close({ target: { classList: { contains() { return false; } } } });
+    aT.setState = () => { };
+    const result = aT.close();
     expect(result).toBe(true);
-    done();
-  }));
-  it('closes the menu and logs in to google', () => new Promise((done) => {
-    document.body.innerHTML = '<button class="googleLogin"/><button class="googleLogout"/>';
-    const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
-    aT.setState = () => {};
-    // @ts-ignore
-    aT.changeNav = () => false;
-    // @ts-ignore
-    aT.loginGoogle = () => true;
-    // @ts-ignore
-    const result = aT.close({
-      target: {
-        classList: {
-          contains(name: string) {
-            if (name === 'loginGoogle') return true;
-            return false;
-          },
-        },
-      },
-    });
-    expect(result).toBe(true);
-    done();
-  }));
-  it('toggles the mobile menu', () => new Promise((done) => {
-    const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
-    // @ts-ignore
-    aT.state.menuOpen = false;
-    aT.setState = (obj) => {
-      // @ts-ignore
-      expect(obj.menuOpen).toBe(true);
-      done();
-    };
-    aT.toggleMobileMenu();
-  }));
-  it('closes the mobile menu on clicking escape key', () => new Promise((done) => {
+  });
+  it('toggles the mobile menu', () => {
+    const { wrapper } = setup();
+    wrapper.instance().setState = jest.fn();
+    wrapper.update();
+    wrapper.instance().toggleMobileMenu();
+    expect(wrapper.instance().setState).toHaveBeenCalled();
+  });
+  it('closes the mobile menu on clicking escape key', () => {
     const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
     aT.setState = jest.fn(() => true);
     const result = aT.handleKeyPress({ key: 'Escape' });
     expect(result).toBe(true);
-    done();
-  }));
-  it('does not closes the mobile menu on clicking Enter key', () => new Promise((done) => {
+  });
+  it('does not closes the mobile menu on clicking Enter key', () => {
     const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
     const result = aT.handleKeyPress({ key: 'Enter' });
     expect(result).toBe(null);
-    done();
-  }));
-  it('toggles the mobile menu on clicking Enter key', () => new Promise((done) => {
+  });
+  it('toggles the mobile menu on clicking Enter key', () => {
     const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
     aT.toggleMobileMenu = () => true;
     const result = aT.handleKeyMenu({ key: 'Enter' });
     expect(result).toBe(true);
-    done();
-  }));
-  it('does not toggle the mobile menu on clicking Escape key', () => new Promise((done) => {
+  });
+  it('does not toggle the mobile menu on clicking Escape key', () => {
     const aT = new AppTemplate({ dispatch: () => Promise.resolve(true) });
     aT.toggleMobileMenu = () => true;
     const result = aT.handleKeyMenu({ key: 'Escape' });
     expect(result).toBe(null);
-    done();
-  }));
+  });
 });
