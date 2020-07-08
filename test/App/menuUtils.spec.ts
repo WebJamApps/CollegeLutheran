03d78b274f0dc796676f39a8526df5b2aba9600d
Rhayven <codingforllamas@gmail.com>
2020-07-07 21:47:40 -0400

    Fixes and Temp workarounds

--- test/App/menuUtils.spec.ts
+++ test/App/menuUtils.spec.ts
@@ -12,32 +12,45 @@ describe('menuUtils', () => {
     },
   };
   it('handles menuItem for Developer', () => {
-    r = menuUtils.menuItem({ link: '/staff', type: 'link', auth: { isAuthenticated: true, user: { userType: 'Developer' } } },
-      1, viewStub);
+    r = menuUtils.menuItem({
+      link: '/staff', type: 'link', auth: true, classname: '', iconClass: '', name: '',
+    },
+    1, viewStub as any);
     expect(r).toBe(null);
   });
   it('handles menuItem for login', () => {
-    r = menuUtils.continueMenuItem({ link: '', type: 'googleLogin', auth: { isAuthenticated: true, user: { userType: 'Developer' } } },
-      1, { pathname: '/staff' }, { isAuthenticated: false, user: { userType: '' } }, viewStub);
+    r = menuUtils.continueMenuItem({
+      link: '', type: 'googleLogin', auth: true, classname: '', iconClass: '', name: '',
+    },
+    1, { pathname: '/staff' }, { isAuthenticated: false, user: { userType: '' } }, viewStub as any);
     expect(r).toBe(true);
   });
   it('handles menuItem for logout', () => {
-    r = menuUtils.continueMenuItem({ link: '', type: 'googleLogout', auth: { isAuthenticated: true, user: { userType: 'Developer' } } },
-      1, { pathname: '/staff' }, { isAuthenticated: true, user: { userType: 'Developer' } }, viewStub);
+    r = menuUtils.continueMenuItem({
+      link: '', type: 'googleLogout', auth: true, classname: '', iconClass: '', name: '',
+    },
+    1, { pathname: '/staff' }, { isAuthenticated: true, user: { userType: 'Developer' } }, viewStub as any);
     expect(r).toBe(true);
   });
   it('hides staff menu item when on staff page', () => {
     viewStub.props.location.pathname = '/staff';
-    r = menuUtils.menuItem({ link: '/staff', type: '', auth: { isAuthenticated: true, user: { userType: 'Developer' } } },
-      1, viewStub);
+    r = menuUtils.menuItem({
+      link: '/staff', type: '', auth: true, classname: '', iconClass: '', name: '',
+    },
+    1, viewStub as any);
     expect(r).toBe(null);
   });
   it('Prevents access to Admin Dashboard when auth user userType is incorrect', () => {
     viewStub.props.auth = { isAuthenticated: true, token: '', user: { userType: 'booya' } };
     r = menuUtils.menuItem({
-      name: 'Admin Dashboard', link: '/admin', type: '', auth: { isAuthenticated: true, user: { userType: 'Developer' } },
+      name: 'Admin Dashboard',
+      link: '/admin',
+      type: '',
+      auth: true,
+      classname: '',
+      iconClass: '',
     },
-    1, viewStub);
+    1, viewStub as any);
     expect(r).toBe(null);
   });
 });
