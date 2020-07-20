2b466ff61bcf29fe31064498a5d0591b5eaf5142
Rhayven <codingforllamas@gmail.com>
2020-07-20 02:59:48 -0400

    Fixing codeClimate issues

--- src/containers/Family/index.tsx
+++ src/containers/Family/index.tsx
@@ -4,9 +4,10 @@ import DefaultFamilyContent from './FamilyContent';
 import mapStoreToProps, { Store } from '../../redux/mapStoreToProps';
 import commonUtils from '../../lib/commonUtils';
 
-export const Family = ({ familyPics }: Store['familyPics']):
-JSX.Element => {
-  commonUtils.setTitleAndScroll('Children & Families', window.screen.width);
+const pageName = ('Children & Families');
+
+export const Family = ({ familyPics }: Store['familyPics']): JSX.Element => {
+  commonUtils.setTitleAndScroll(`${pageName}`, window.screen.width);
   return (<DefaultFamilyContent familyPics={familyPics} />);
 };
 
