f4d74f5f1b537995cd06c304e67ae688ab3340e0
Joshua Sherman <joshua.v.sherman@gmail.com>
2020-12-08 08:21:38 -0500

    fix linter

--- src/containers/LiveStream/index.tsx
+++ src/containers/LiveStream/index.tsx
@@ -5,7 +5,7 @@ const LiveStream = (): JSX.Element => {
   commonUtils.setTitleAndScroll('Livestream', window.screen.width);
   return (
     <div style={{ margin: 'auto', width: '98%', textAlign: 'center' }}>
-      <p style={{marginBottom: '0px' }}>This video is available during a live streaming event.</p>
+      <p style={{ marginBottom: '0px' }}>This video is available during a live streaming event.</p>
       <iframe
         title="College Lutheran Church Youtube"
         width="100%"
