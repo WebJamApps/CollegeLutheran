4b6500e9b1f8684604bcecb1b715a0f83cd655c0
Rhayven <codingforllamas@gmail.com>
2021-01-05 17:10:39 -0500

    Removes redundant test

--- test/containers/livestream.spec.tsx
+++ test/containers/livestream.spec.tsx
@@ -12,13 +12,4 @@ function setup() {
 describe('LiveStream', () => {
   const { wrapper } = setup();
   it('renders snapshot correctly', () => { expect(wrapper).toMatchSnapshot(); });
-  it('Changes the URLs on production', () => {
-    let twitchUrl = '',
-      chatUrl = '';
-    if (process.env.NODE_ENV === 'production') {
-      twitchUrl = 'https://player.twitch.tv/?channel=collegelutheranchurch&parent=collegelutheran.com/livestream';
-      chatUrl = 'https://player.twitch.tv/?channel=collegelutheranchurch&parent=collegelutheran.com/livestream';
-    }
-    expect(twitchUrl).toBe('');
-  });
 });
