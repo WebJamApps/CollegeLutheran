1218743f0b3dcd36aa745b63d65385d887308301
Joshua Sherman <joshua.v.sherman@gmail.com>
2021-01-07 09:20:01 -0500

    react resize detector

--- src/containers/LiveStream/index.tsx
+++ src/containers/LiveStream/index.tsx
@@ -1,16 +1,20 @@
-
 import React from 'react';
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
-
+import { withResizeDetector } from 'react-resize-detector';
 import commonUtils from '../../lib/commonUtils';
 
-const LiveStream = (): JSX.Element => {
+export interface LiveStreamProps {
+  width:number, height:number
+}
+const LiveStream = ({ width, height }:LiveStreamProps): JSX.Element => {
   commonUtils.setTitleAndScroll('Livestream', window.screen.width);
+  console.log(`width: ${width}`);
+  console.log(`height: ${height}`);
   return (
-    <div style={{ margin: 'auto', width: '98%', textAlign: 'center' }}>
+    <div style={{ margin: 'auto', width: '100%', textAlign: 'center' }}>
       <h4>Livestream</h4>
       <ReactTwitchEmbedVideo channel="collegelutheranchurch" />
     </div>
   );
 };
-export default LiveStream;
+export default withResizeDetector(LiveStream);
