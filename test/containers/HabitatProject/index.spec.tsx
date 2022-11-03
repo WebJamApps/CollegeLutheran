6c78597572b4e93d1ef9b2a2d16336bbdd07bc93
Kathleen Hall <kathleenahall22@gmail.com>
2022-11-03 11:39:45 -0400

    added unit test for HabitatProject and unit test for fetch.ts

--- test/containers/HabitatProject/index.spec.tsx
+++ test/containers/HabitatProject/index.spec.tsx
@@ -1,8 +1,6 @@
-
-import React from 'react';
 import renderer from 'react-test-renderer';
-import HabitatProject from '../../src/containers/HabitatProject/index';
-import DefaultHabitatContent from '../../src/containers/HabitatProject/HabitatProjectContent';
+import HabitatProject from 'src/containers/HabitatProject/index';
+import DefaultHabitatContent from 'src/containers/HabitatProject/HabitatProjectContent';
 
 describe('Habitat Project', () => {
   it('Renders the Habitat Project page', () => {
