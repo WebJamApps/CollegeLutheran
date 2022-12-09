05b976f9f3095eb85e638988c1e76a34537b915c
Kathleen Hall <kathleenahall22@gmail.com>
2022-12-08 19:31:43 -0500

    fetched pics using axios

--- src/Providers/PicsProvider.tsx
+++ src/Providers/PicsProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactChild, useEffect } from 'react';
 import createPersistedState from 'use-persisted-state';
-import fetch from 'src/lib/fetch';
+// import fetch from 'src/lib/fetch';
 import axios from 'axios';
 
 export interface Ipicture {
@@ -25,54 +25,44 @@ export interface Ipicture {
   checkedOutByName?: string,
 }
 
-export interface IpictureTypes {
-  musicPics: Ipicture[], familyPics: Ipicture[], youthPics: Ipicture[], habitatPics: Ipicture[], otherPics: Ipicture[],
-}
+const populatePictures = async (setPictures: any) => {
+  const { data: musicPics } = await axios.get(`${process.env.BackendUrl}/book?type=musicPics`);
+  const pictures = { musicPics };
+  // console.log(pictures);
+  setPictures(pictures);
+};
 
-async function populatePictures(this: any, setPictures: any) {
-  // call set pictures with array of objects, use this.fetch but use axios instead of superagent **use try catch for fetch calls
-  // const buildAxiosFetch = await axios.fetch
-  if (setPictures.type === '') setPictures.type = ['musicPics', 'familyPics', 'youthPics', 'habitatPics', 'otherPics'];
-  try {
-    this.fetch.fetchGet(axios, 'book/one?type=homePageContent', 'GOT_HOMEPAGE');
-    this.fetch.fetchGet(axios, 'book/one?type=youthPageContent', 'GOT_YOUTHPAGE');
-    this.fetch.fetchGet(axios, 'book?type=familyPics', 'GOT_FAMILYPICS');
-    this.fetch.fetchGet(axios, 'book?type=Forum', 'GOT_BOOKS');
-    this.fetch.fetchGet(axios, 'book?type=youthPics', 'GOT_YOUTHPICS');
-    this.fetch.fetchGet(axios, 'book?type=otherPics&type=habitat', 'GOT_OTHERPICS');
-    this.fetch.fetchGet(axios, 'book?type=musicPics', 'GOT_MUSICPICS');
-  } catch (error) {
-    console.log(error);
-  }
+export interface IpictureTypes {
+  musicPics: Ipicture[], familyPics: Ipicture[], youthPics: Ipicture[],
+  habitatPics: Ipicture[], otherPics: Ipicture[],
 }
 
 const usePictureState: (arg0: IpictureTypes) =>
 [IpictureTypes, (arg0: IpictureTypes) => void] = createPersistedState('pictures', sessionStorage);
 
 export const PictureContext = createContext({
-  pictures: {
-    musicPics: [], familyPics: [], youthPics: [], habitatPics: [], otherPics: [],
-  },
-  setPictures: () => { },
+  pictures: {} as IpictureTypes,
+  setPictures: (_arg0: IpictureTypes) => { },
   getPictures: () => Promise.resolve(),
 });
 
 type Props = { children: ReactChild };
 export function PictureProvider({ children }: Props): JSX.Element {
   const { Provider } = PictureContext;
-  const [pictures, setPictures] = usePictureState({
-    musicPics: [], familyPics: [], youthPics: [], habitatPics: [], otherPics: [],
-  });
+  const [pictures, setPictures] = usePictureState({} as IpictureTypes);
+
+  const getPictures = async () => populatePictures(setPictures);
 
   useEffect(() => {
     (async () => {
       await populatePictures(setPictures);
     })();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <Provider value={{
-      getPictures, setPictures, pictures,
+      setPictures, pictures, getPictures,
     }}
     >
       {children}
