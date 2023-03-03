25e969ae35b0458ca8c025301e2a2eb97a6418ad
Kathleen Hall <kathleenahall22@gmail.com>
2023-03-03 08:25:52 -0500

    replaced superagent with axios in Admin Dash-utils

--- src/containers/AdminDashboard/utils.tsx
+++ src/containers/AdminDashboard/utils.tsx
@@ -1,27 +1,35 @@
 import type { Iauth } from 'src/providers/Auth.provider';
-import superagent from 'superagent';
 import Fetch from 'src/lib/fetch';
 import commonUtils from 'src/lib/commonUtils';
 import type { AnyAction, Dispatch } from 'redux';
+import axios from 'axios';
 
-async function putAPI(
-  body: { title: string; comments: string; type: string },
+async function putAPI(// used to update the text the homepage or on the youthpage
+  data: { title: string; comments: string; type: string },
   auth: Iauth, dispatch: Dispatch<AnyAction>,
 ): Promise<void> {
   try {
-    const res = await superagent.put(`${process.env.BackendUrl}/book/one?type=${body.type}`)
-      .set('Authorization', `Bearer ${auth.token}`).set('Accept', 'application/json')
-      .send(body);
-    if (res.status === 200) {
+    const config = {
+      url: `${process.env.BackendUrl}/book/one?type=${data.type}`,
+      method: 'put',
+      headers: { Authorization: `Bearer ${auth.token}`, Accept: 'application/json' },
+      data,
+    };
+    const { status } = await axios.request(config);
+    if (status === 200) {
       await Fetch.fetchGet(dispatch, 'book/one?type=homePageContent', 'GOT_HOMEPAGE');
       commonUtils.notify('Homepage', 'sucessfully updated', 'success');
     }
   } catch (e) {
-    if ((e as any).response.status === 400) {
+    if ((e as any).status === 400) {
       try {
-        await superagent.post(`${process.env.BackendUrl}/book`)
-          .set('Authorization', `Bearer ${auth.token}`).set('Accept', 'application/json')
-          .send(body);
+        const config = {
+          url: `${process.env.BackendUrl}/book`,
+          method: 'post',
+          headers: { Authorization: `Bearer ${auth.token}`, Accept: 'application/json' },
+          data,
+        };
+        await axios.request(config);
       } catch (err) {
         console.log((err as Error).message);
       }
@@ -32,23 +40,23 @@ async function putAPI(
 async function addNewsAPI(
   auth: Iauth,
   dispatch: Dispatch<AnyAction>, clearForm: () => void,
-  body: { title: string, url: string, comments: string },
+  data: { title: string, url: string, comments: string },
 ): Promise<void> {
   try {
-    await superagent.post(
-      `${process.env.BackendUrl}/book`).set('Authorization', `Bearer ${auth.token}`,
-    )
-      .set('Accept', 'application/json')
-      .send({
-        ...body,
-        type: 'Forum',
-        access: 'CLC',
-      });
+    const config = {
+      url: `${process.env.BackendUrl}/book`,
+      method: 'post',
+      headers: { Authorization: `Bearer ${auth.token}`, Accept: 'application/json' },
+      data,
+      type: 'Forum',
+      access: 'CLC',
+    };
+    await axios.request(config);
     await Fetch.fetchGet(dispatch, 'book?type=Forum', 'GOT_BOOKS');
     clearForm();
-    commonUtils.notify(body.title, 'Successfully added news', 'success');
+    commonUtils.notify(data.title, 'Successfully added news', 'success');
   } catch (e) {
-    commonUtils.notify(body.title, `Failed to add news, ${(e as Error).message}`, 'warning');
+    commonUtils.notify(data.title, `Failed to add news, ${(e as Error).message}`, 'warning');
   }
 }
 
@@ -57,9 +65,13 @@ async function createPicAPI(
   pic: Record<string, unknown>, auth: Iauth,
 ): Promise<void> {
   try {
-    await superagent.post(`${process.env.BackendUrl}/book`)
-      .set('Authorization', `Bearer ${auth.token}`).set('Accept', 'application/json')
-      .send(pic);
+    const config = {
+      url: `${process.env.BackendUrl}/book`,
+      method: 'post',
+      headers: { Authorization: `Bearer ${auth.token}`, Accept: 'application/json' },
+      pic,
+    };
+    await axios.request(config);
     await getPictures();
     setShowDialog(false);
   } catch (e) { console.log((e as Error).message); }
--- test/containers/family.spec.tsx
+++ test/containers/family.spec.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import renderer from 'react-test-renderer';
+import PicSlider from 'src/components/PicSlider';
+import FamilyContent from 'src/containers/Family/FamilyContent';
 import { Family } from '../../src/containers/Family/index';
 
 describe('Family', () => {
