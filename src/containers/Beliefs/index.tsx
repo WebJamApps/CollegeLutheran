f5059edff979b112438918ca6602c8dc551cd671
Kathleen Hall <kathleenahall22@gmail.com>
2023-03-17 15:47:45 -0400

    fixed belief index

--- src/containers/Beliefs/index.tsx
+++ src/containers/Beliefs/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import DefaultBeliefsContent from './BeliefsContent';
 import commonUtils from '../../lib/commonUtils';
 
-const Staff = (): JSX.Element => {
+export const Beliefs = (): JSX.Element => {
   commonUtils.setTitleAndScroll('Our Lutheran Beliefs', window.screen.width);
   return (<DefaultBeliefsContent />);
 };
-export default Staff;
