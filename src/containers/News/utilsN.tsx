5f901d6d679e10fcd5208bad2ee003c982b4d240
Kathleen Hall <kathleenahall22@gmail.com>
2023-08-10 09:46:28 -0400

    fixed cc

--- src/containers/News/utilsN.tsx
+++ src/containers/News/utilsN.tsx
@@ -6,16 +6,18 @@ export const defaultNews = {
   url: '', comments: '', title: '', _id: '', type: 'Forum',
 } as Ibook;
 
-async function updateNews(
+async function performAxiosRequest(
+  method: 'put' | 'delete',
+  url: string,
   editNews: typeof defaultNews,
   auth: Iauth,
   getNews: () => Promise<void>,
-  setEditNews: (arg0:typeof defaultNews) => void,
+  setEditNews: (arg0: typeof defaultNews) => void,
 ): Promise<void> {
   try {
     const config = {
-      url: `${process.env.BackendUrl}/book/${editNews._id}`,
-      method: 'put',
+      url,
+      method,
       headers: { Authorization: `Bearer ${auth.token}`, Accept: 'application/json' },
       data: editNews,
     };
@@ -24,7 +26,19 @@ async function updateNews(
       setEditNews(defaultNews);
       await getNews();
     }
-  } catch (err) { console.log((err as Error).message); }
+  } catch (err) {
+    console.log((err as Error).message);
+  }
+}
+
+async function updateNews(
+  editNews: typeof defaultNews,
+  auth: Iauth,
+  getNews: () => Promise<void>,
+  setEditNews: (arg0:typeof defaultNews) => void,
+): Promise<void> {
+  const url = `${process.env.BackendUrl}/book/${editNews._id}`;
+  await performAxiosRequest('put', url, editNews, auth, getNews, setEditNews);
 }
 
 async function deleteNews(
@@ -33,19 +47,8 @@ async function deleteNews(
   auth: Iauth,
   setEditNews: (arg0: typeof defaultNews) => void,
 ): Promise<void> {
-  try {
-    const config = {
-      url: `${process.env.BackendUrl}/book/${editNews._id}`,
-      method: 'delete',
-      headers: { Authorization: `Bearer ${auth.token}`, Accept: 'application/json' },
-      data: editNews,
-    };
-    const { status } = await axios.request(config);
-    if (status === 200) {
-      setEditNews(defaultNews);
-      await getNews();
-    }
-  } catch (err) { console.log((err as Error).message); }
+  const url = `${process.env.BackendUrl}/book/${editNews._id}`;
+  await performAxiosRequest('delete', url, editNews, auth, getNews, setEditNews);
 }
 
 const makeShowHideBulletin = (setNews: (arg0:typeof defaultNews) => void,
