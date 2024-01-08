eb6ae25cff10defa5fa55a6c3797f80755401688
Kat Hall <kathleenahall22@gmail.com>
2024-01-07 19:22:42 -0500

    some unit tests

--- test/containers/AdminDashboard/EditPic/EditPicDialog.spec.tsx
+++ test/containers/AdminDashboard/EditPic/EditPicDialog.spec.tsx
@@ -10,4 +10,38 @@ describe('EditPicDialog', () => {
     const epd = renderer.create(<EditPicDialog {...props} />).toJSON();
     expect(epd).toMatchSnapshot();
   });
+  it('handles events for EditPicDialog for url field', () => {
+    const props = {
+      onClose: jest.fn(), editPic: defaultPic, setEditPic: jest.fn(),
+    };
+    const evt = { target: { value: 'url' } };
+    const result = renderer.create(<EditPicDialog {...props} />).root;
+    expect(result.findByProps({ label: '* URL' }).props.onChange(evt)).toBe('url');
+  });
+  it('handles events for EditPicDialog for title field', () => {
+    const props = {
+      onClose: jest.fn(), editPic: defaultPic, setEditPic: jest.fn(),
+    };
+    const evt = { target: { value: 'title' } };
+    const result = renderer.create(<EditPicDialog {...props} />).root;
+    expect(result.findByProps({ label: '* Title' }).props.onChange(evt)).toBe('title');
+  });
+  it('handles onClose for EditPicDialog', () => {
+    const props = {
+      editPic: defaultPic, onClose: jest.fn(), setEditPic: jest.fn(),
+    };
+    const result = renderer.create(<EditPicDialog {...props} />).root;
+    result.findByProps({ className: 'editPicDialog' }).props.onClose();
+    expect(props.setEditPic).toHaveBeenCalled();
+  });
+  // it('handles onClick for EditPicDialog', () => {
+  //   picUtils.updatePic = jest.fn();
+  //   picUtils.deletePic = jest.fn();
+  //   const props = {
+  //     editPic: defaultPic, onClose: jest.fn(), setEditPic: jest.fn(),
+  //   };
+  //   const result = renderer.create(<EditPicDialog {...props} />).root;
+  //   result.findByProps({ className: 'updatePicButton' }).props.onClick();
+  //   expect(picUtils.updatePic).toHaveBeenCalled();
+  // });
 });
