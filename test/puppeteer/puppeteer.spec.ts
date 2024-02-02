d589befe84711f0b8a8057442a829be6b15c2750
3-Part-Breath <dogledor@gmail.com>
2024-02-02 12:03:43 -0500

    news page test
    Added test to navigate to news page. will need to change when puppeteer sidebar rendering is figured out

--- test/puppeteer/puppeteer.spec.ts
+++ test/puppeteer/puppeteer.spec.ts
@@ -4,9 +4,8 @@ describe('Puppeteer tests', () => {
   let browser: any, page: any;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: false, args: ['--window-size=1280,780'] });
+    browser = await puppeteer.launch({ headless: false, args: ['--window-size=1280,720'] });
     page = await browser.newPage();
-    // await page.setViewport({ width: 1280, height: 720 });
   });
 
   afterAll(async () => {
@@ -26,12 +25,22 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to College Lutheran Beliefs', async () => {
-    await page.goto('https://www.collegelutheran.org/');
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'domcontentloaded' });
     await page.waitForSelector('.nav-link');
     const pageButton = await page.$('.nav-link');
     await pageButton.click();
     const title = await page.title();
-    expect(title).toBe('Our Lutheran Beliefs');
+    expect(title).toBe('Our Lutheran Beliefs | College Lutheran Church');
+  });
+  it('should load College Lutheran News', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'domcontentloaded' });
+    const newsLink = await page.waitForXPath('//a[contains(., "News")]');
+    await Promise.all([
+      newsLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('News | College Lutheran Church');
   });
 // eslint-disable-next-line eol-last
 });
