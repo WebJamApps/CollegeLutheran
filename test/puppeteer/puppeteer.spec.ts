32700a7bf9d948dc0c3a3632edb8f0ecfce9bf23
3-Part-Breath <dogledor@gmail.com>
2024-02-02 13:03:34 -0500

    Added tests for all test page navigation
    except bulletin

--- test/puppeteer/puppeteer.spec.ts
+++ test/puppeteer/puppeteer.spec.ts
@@ -4,8 +4,13 @@ describe('Puppeteer tests', () => {
   let browser: any, page: any;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: false, args: ['--window-size=1280,720'] });
+    browser = await puppeteer.launch({ headless: 'new' });
     page = await browser.newPage();
+    await page.setViewport({
+      width: 1280,
+      height: 720,
+      deviceScaleFactor: 1,
+    });
   });
 
   afterAll(async () => {
@@ -24,17 +29,55 @@ describe('Puppeteer tests', () => {
     expect(title).toBe('College Lutheran Church');
   });
 
-  it('navigate to College Lutheran Beliefs', async () => {
-    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'domcontentloaded' });
-    await page.waitForSelector('.nav-link');
-    const pageButton = await page.$('.nav-link');
-    await pageButton.click();
+  it('navigate to Beliefs', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Our Lutheran Beliefs")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
     const title = await page.title();
     expect(title).toBe('Our Lutheran Beliefs | College Lutheran Church');
   });
-  it('should load College Lutheran News', async () => {
-    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'domcontentloaded' });
-    const newsLink = await page.waitForXPath('//a[contains(., "News")]');
+
+  it('navigate to Staff', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Church Staff")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Church Staff | College Lutheran Church');
+  });
+
+  // eslint-disable-next-line jest/no-commented-out-tests
+  /* it('navigate to Bulletin', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Bulletin")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Church Staff | College Lutheran Church');
+  });
+  */
+
+  it('navigate to LiveStream', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Livestream")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Livestream | College Lutheran Church');
+  });
+
+  it('navigate to News', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const newsLink = await page.waitForXPath('//span[contains(., "News")]');
     await Promise.all([
       newsLink.evaluate((el: { click: () => any; }) => el.click()),
       page.waitForNavigation(),
@@ -42,5 +85,50 @@ describe('Puppeteer tests', () => {
     const title = await page.title();
     expect(title).toBe('News | College Lutheran Church');
   });
+
+  it('navigate to Giving', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Giving")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Giving | College Lutheran Church');
+  });
+
+  it('navigate to Music', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Music")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Music | College Lutheran Church');
+  });
+
+  it('navigate to Youth Ministry', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Youth Ministry")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Youth Ministry | College Lutheran Church');
+  });
+
+  it('navigate to Children & Families', async () => {
+    await page.goto('https://www.collegelutheran.org/', { waitUntil: 'networkidle0' });
+    const belLink = await page.waitForXPath('//span[contains(., "Children & Families")]');
+    await Promise.all([
+      belLink.evaluate((el: { click: () => any; }) => el.click()),
+      page.waitForNavigation(),
+    ]);
+    const title = await page.title();
+    expect(title).toBe('Children & Families | College Lutheran Church');
+  });
+
 // eslint-disable-next-line eol-last
 });
