36689b43ecedb80441415d7f2fb1125ebfce85e6
JoshuaVSherman <joshua.v.sherman@gmail.com>
2024-02-17 11:22:28 -0500

    provides more puppeting

--- test/puppeteer/puppeteer.spec.ts
+++ test/puppeteer/puppeteer.spec.ts
@@ -1,10 +1,18 @@
 import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const delay = (seconds: number) => new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 
 describe('Puppeteer tests', () => {
   let browser: any, page: any;
-  const port = Number(process.env.PORT) + 10;
+  const url = process.env.PUPPET_URL || 'http://localhost:7787';
+  const headless = !process.env.HEADLESS || process.env.HEADLESS === 'new' ? 'new' : false;
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: Boolean(process.env.HEADLESS) });
+    console.log('headless?');
+    console.log(process.env.HEADLESS);
+    browser = await puppeteer.launch({ headless });
     page = await browser.newPage();
     await page.setViewport({
       width: 1280,
@@ -13,18 +21,23 @@ describe('Puppeteer tests', () => {
     });
   });
 
+  afterEach(async () => {
+    if (!headless) {
+      await delay(2);
+    }
+  });
+
   afterAll(async () => {
     await browser.close();
   });
 
   it('should load College Lutheran Homepage', async () => {
-    await page.goto(`https://localhost:${port}`);
+    await page.goto(`${url}`);
     const title = await page.title();
     expect(title).toBe('College Lutheran Church');
   });
 
   it('navigate to Beliefs', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Our Lutheran Beliefs")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -35,7 +48,6 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to Staff', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Church Staff")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -59,7 +71,6 @@ describe('Puppeteer tests', () => {
   */
 
   it('navigate to LiveStream', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Livestream")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -70,7 +81,6 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to News', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const newsLink = await page.waitForXPath('//span[contains(., "News")]');
     await Promise.all([
       newsLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -81,7 +91,6 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to Giving', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Giving")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -92,7 +101,6 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to Music', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Music")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -103,7 +111,6 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to Youth Ministry', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Youth Ministry")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -114,7 +121,6 @@ describe('Puppeteer tests', () => {
   });
 
   it('navigate to Children & Families', async () => {
-    await page.goto(`https://localhost:${port}`, { waitUntil: 'networkidle0' });
     const belLink = await page.waitForXPath('//span[contains(., "Children & Families")]');
     await Promise.all([
       belLink.evaluate((el: { click: () => any; }) => el.click()),
@@ -123,6 +129,4 @@ describe('Puppeteer tests', () => {
     const title = await page.title();
     expect(title).toBe('Children & Families | College Lutheran Church');
   });
-
-// eslint-disable-next-line eol-last
 });
