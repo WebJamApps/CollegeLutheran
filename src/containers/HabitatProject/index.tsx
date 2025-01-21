3c7a32f0ecac0dfe5cfd2cc7dbda26a285cba207
JoshuaVSherman <joshua.v.sherman@gmail.com>
2025-01-21 17:55:02 -0500

    removed stewardship added habitat but wrong page content

--- src/containers/HabitatProject/index.tsx
+++ src/containers/HabitatProject/index.tsx
@@ -1,8 +1,8 @@
-// import HabitatProjectContent from './HabitatProjectContent';
-// import commonUtils from '../../lib/commonUtils';
+import HabitatProjectContent from './HabitatProjectContent';
+import commonUtils from '../../lib/commonUtils';
 
-// const HabitatProject = (): JSX.Element => {
-//   commonUtils.setTitleAndScroll('Habitat Project', window.screen.width);
-//   return (<HabitatProjectContent />);
-// };
-// export default HabitatProject;
+const HabitatProject = (): JSX.Element => {
+  commonUtils.setTitleAndScroll('Habitat Project', window.screen.width);
+  return (<HabitatProjectContent />);
+};
+export default HabitatProject;
